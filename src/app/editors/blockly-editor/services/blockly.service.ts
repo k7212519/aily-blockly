f905f1c9ac9e02db27c49ef8ca9103f96e58b146
downey <853384781@qq.com>
2026-03-18 14:21:34 +0800

    移除不存在库兼容

--- src/app/editors/blockly-editor/services/blockly.service.ts
+++ src/app/editors/blockly-editor/services/blockly.service.ts
@@ -315,7 +315,9 @@ export class BlocklyService {
       if (block.type && Blockly.Blocks[block.type]) {
         console.log(`- delete ${block.type}`);
         delete Blockly.Blocks[block.type];
-        delete (window as any).Arduino.forBlock[block.type];
+        if ((window as any).Arduino.forBlock[block.type]) {
+          delete (window as any).Arduino.forBlock[block.type];
+        }
       }
     }
   }
